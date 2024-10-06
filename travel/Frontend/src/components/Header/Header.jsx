563c9c23bc7472a67d2de94cdff46412930f1688
Sarthaksl <sarthakharade166@gmail.com>
2024-10-06 22:10:03 +0530

    new changes added

--- travel/Frontend/src/components/Header/Header.jsx
+++ travel/Frontend/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useContext} from 'react';
 import { Container, Row, Button } from 'reactstrap';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
 
 import new_logo from '../../assets/images/new_logo.png'; // Corrected path
 import './header.css';
+import { AuthContext } from '../../context/AuthContext';
 
 const nav__links = [
   {
@@ -23,6 +24,15 @@ const nav__links = [
 const Header = () => {
 
   const headerRef = useRef(null);
+  const menuRef = useRef(null);
+  const navigate = useNavigate()
+  const {user, dispatch} = useContext(AuthContext)
+
+  const logout = ()=>{
+    dispatch({type: 'LOGOUT'})
+    navigate('/')
+  }
+
 
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', () => {
@@ -37,10 +47,11 @@ const Header = () => {
   useEffect(() => {
     stickyHeaderFunc();
 
+
     return window.removeEventListener('scroll', stickyHeaderFunc)
-  }) 
+  }) ;
 
-
+  const toggleMenu = () => menuRef.current.classList.toggle('show_menu')
   return (
     <header className='header' ref={headerRef}>
       <Container>
@@ -51,7 +62,7 @@ const Header = () => {
               <img src={new_logo} alt='' />
             </div>
 
-            <div className='navigation'>
+            <div className='navigation' ref={menuRef} onClick={toggleMenu}>
               <ul className='menu d-flex align-items-center gap-5'>
                 {nav__links.map((item, index) => (
                   <li className='nav__item' key={index}>
@@ -70,14 +81,26 @@ const Header = () => {
 
             <div className='nav__right d-flex align-items-center gap-4'>
               <div className='nav__btns d-flex align-items-center gap-4'>
-                <Button className='btn secondary__btn'>
+                 {
+                  user? (<>
+                  <h5 className="mb-0">{user.username}</h5>
+                  <Button className="btn btn-dark" onClick={logout}>Logout</Button>
+                  
+                  </>
+                  ):(
+                   <>
+                  <Button className='btn secondary__btn'>
                   <Link to='/login'>Login</Link>
                 </Button>
                 <Button className='btn primary__btn'>
                   <Link to='/register'>Register</Link>
                 </Button>
+                  
+                  </>
+                 )}
+              
               </div>
-              <span className='mobile__menu'>
+              <span className='mobile__menu' onClick={toggleMenu}>
                 <i className='ri-menu-line'></i>
               </span>
             </div>
--- travel/Frontend/src/pages/Tours.jsx
+++ travel/Frontend/src/pages/Tours.jsx
@@ -5,20 +5,27 @@ import "../styles/tours.css"
 import TourCard from '../shared/TourCard'
 import SearchBar from '../shared/SearchBar'
 import Newsletter from '../shared/Newsletter'
-import tourData from '../assets/data/tours'
+
 import { Container,Row,Col } from 'reactstrap';
+
+import useFetch from '../hooks/useFetch';
+import { BASE_URL } from '../utils/config';
 
 const Tours = () => {
 
   const [pageCount, setPageCount] = useState(0)
   const [page, setPage] = useState(0)
+
+  const {data:tours, loading, error} = useFetch(`${BASE_URL}/tours?page=${page}`);
+  const {data:tourCount} = useFetch(`${BASE_URL}/tours/search/getTourCount`);
   
   useEffect(() => {
 
-    const pages = Math.ceil(5 / 4)
+    const pages = Math.ceil(tourCount / 8);
     setPageCount(pages)
+    window.scrollTo(0,0)
 
-  }, [page] )
+  }, [page, tourCount, tours] );
 
   return (
     <>
@@ -33,23 +40,29 @@ const Tours = () => {
 
     <section className='pt-0'>
       <Container>
-        <Row>
-          {tourData?.map((tour => (
-            <Col lg="3" className="mb-4" key={tour.id}>
-              <TourCard tour={tour}/>
-            </Col>
-          )))}
 
-          <Col lg="12">    // this section is to be used for pagination(to show on which page you currently are on)  
-            <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
-              {[...Array(pageCount),keys()].map(number => { 
-                <span key={number} onClick={() => setPage(number)} className={page=== number ? "active__page" : ""}>
-                  {number + 1}
-                </span>
-              })}
-            </div>
+        {loading && <h4 className="text-center pt-5">Loading.....</h4>}
+        {error && <h4 className="text-center pt-5">{error} </h4>}
+       {
+        !loading && !error && (
+         <Row>
+        {tours?.map((tour => (
+          <Col lg="3" className="mb-4" key={tour.id}>
+            <TourCard tour={tour}/>
           </Col>
-        </Row>
+        )))}
+
+        <Col lg="12">    // this section is to be used for pagination(to show on which page you currently are on)  
+          <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+            {[...Array(pageCount),keys()].map(number => { 
+              <span key={number} onClick={() => setPage(number)} className={page=== number ? "active__page" : ""}>
+                {number + 1}
+              </span>
+            })}
+          </div>
+        </Col>
+      </Row>)
+       }
       </Container>
     </section>
 
@@ -57,7 +70,7 @@ const Tours = () => {
 
     
     </>
-  )
-}
+  );
+};
 
 export default Tours
